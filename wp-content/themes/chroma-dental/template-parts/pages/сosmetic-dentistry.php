81c46a0476e77609f628520d2a8863b1696b77eb
a.sivak <riziy_bes@mail.ru>
2020-11-09 11:13:13 +0300

    created first section fo cosmetic dentistry page

--- wp-content/themes/chroma-dental/template-parts/pages/сosmetic-dentistry.php
+++ wp-content/themes/chroma-dental/template-parts/pages/сosmetic-dentistry.php
@@ -1,2 +1,15 @@
-<?php
-get_template_part('template-parts/blocks/reviews');
+<div class="cosmetic-dentistry">
+
+	<section class="first-section">
+		<div class="container">
+			<h2 class="first-section__caption">STUDIO FOR AESTHETIC DENTISTRY</h2>
+			<p class="first-section__text">Toronto’s first spa-concept environment. <br>
+				A truly unique environment dedicated to giving you the smile makeover you’ve always wanted in the comfort of a private studio setting. Where the services are exclusive and often focus on cosmetic dentistry.</p>
+
+			<?php get_template_part('template-parts/components/order-free-consultation'); ?>
+		</div>
+	</section>
+
+	<?php get_template_part('template-parts/blocks/reviews'); ?>
+</div>
+
