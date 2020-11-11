2abbdd4887f626109c3fd2faea7ab06d7bb5569a
a.sivak <riziy_bes@mail.ru>
2020-11-11 17:26:34 +0300

    update our team block

--- wp-content/themes/chroma-dental/template-parts/components/our-team.php
+++ wp-content/themes/chroma-dental/template-parts/components/our-team.php
@@ -1,29 +1,30 @@
 <?php $our_team = get_field('about_team', 'option'); ?>
 
 <section class="our-team container">
-	<div class="our-team__wrap">
-		<div class="our-team__content">
-			<div class="octagon our-team__photo">
+
+  <div class="our-team__photo">
+    <div class="octagon">
           <span class="octagon_item octagon_top">
             <span class="corner"></span>
           </span>
-				<span class="octagon_item octagon_rt">
+      <span class="octagon_item octagon_rt">
             <span class="corner"></span>
           </span>
-				<span class="octagon_item octagon_lt">
+      <span class="octagon_item octagon_lt">
             <span class="corner"></span>
           </span>
-				<span class="octagon_item octagon_bottom">
+      <span class="octagon_item octagon_bottom">
             <span class="corner"></span>
           </span>
-				<figure class="our-team__photo_wrap">
-					<img src="<?= $our_team['photo']; ?>" alt="img" class="our-team__photo_img">
-				</figure>
-			</div>
-			<h2 class="our-team__caption"><?= $our_team['caption']; ?></h2>
-			<p class="our-team__text"><?= $our_team['text']; ?></p>
+      <figure class="our-team__photo_wrap">
+        <img src="<?= $our_team['photo']; ?>" alt="img" class="our-team__photo_img">
+      </figure>
+    </div>
+  </div>
+  <div class="our-team__content">
+    <h2 class="our-team__caption"><?= $our_team['caption']; ?></h2>
+    <p class="our-team__text"><?= $our_team['text']; ?></p>
 
-			<?php get_template_part('template-parts/components/order-free-consultation'); ?>
+    <?php get_template_part('template-parts/components/order-free-consultation'); ?>
 		</div>
-	</div>
 </section>
